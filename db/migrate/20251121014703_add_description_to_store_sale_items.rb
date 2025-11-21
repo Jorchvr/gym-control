79bc97612d7e379ac45b14462687ccad91177fe0
Jorchvr <jgael_vr@hotmail.com>
2025-11-21 01:42:59 -0600

    Agrego columna description a store_sale_items para servicios Griselle

--- db/migrate/20251121014703_add_description_to_store_sale_items.rb
+++ db/migrate/20251121014703_add_description_to_store_sale_items.rb
@@ -1,3 +1,4 @@
+# db/migrate/20251121014703_add_description_to_store_sale_items.rb
 class AddDescriptionToStoreSaleItems < ActiveRecord::Migration[7.1]
   def change
     add_column :store_sale_items, :description, :string
